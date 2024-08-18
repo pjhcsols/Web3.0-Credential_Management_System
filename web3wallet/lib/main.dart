89f74ce9326d85eee25834927a10253e2f791e8e
Seah Kim <seakim@knu.ac.kr>
2024-08-18 12:55:31 +0900

    design: add main page card

--- web3wallet/lib/main.dart
+++ web3wallet/lib/main.dart
@@ -13,20 +13,11 @@ class WalletCard extends StatefulWidget {
 
 class _MainPageState extends State<WalletCard> {
   int _selectedIndex = 0;
-  final List<Map<String, String>> _cards = [
-    {"title": "경북대학교 학생증", "description": "테스트 입니다"},
-    {"title": "국제학생증", "description": "테스트 입니다"},
-  ];
-
-  static const List<Widget> _placeholders = <Widget>[
-    Placeholder(),
-    Placeholder(),
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _selectedIndex == 0 ? VerticalCardSlider(cards: _cards) : _placeholders[_selectedIndex - 1],
+      body: _selectedIndex == 0 ? const CardDisplay() : const Placeholder(),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -43,59 +34,84 @@ class _MainPageState extends State<WalletCard> {
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
+        selectedItemColor: const Color(0xFFE60000),
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
       ),
     );
   }
 }
 
-class VerticalCardSlider extends StatelessWidget {
-  final List<Map<String, String>> cards;
-
-  const VerticalCardSlider({Key? key, required this.cards}) : super(key: key);
+class CardDisplay extends StatelessWidget {
+  const CardDisplay({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: const Text("Web 3 Wallet"),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: cards.length,
-          itemBuilder: (context, index) {
-            return Card(
-              margin: const EdgeInsets.symmetric(vertical: 10.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16.0),
+        padding: const EdgeInsets.all(32.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                offset: Offset(0, 0),
+                blurRadius: 10,
+                spreadRadius: 1,
               ),
-              child: Container(
-                height: 240.0,
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      cards[index]['title']!,
-                      style: const TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 10.0),
-                    Text(
-                      cards[index]['description']!,
+            ],
+          ),
+          child: AspectRatio(
+            aspectRatio: 2 / 3,
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 24.0),
+                  const Center(
+                    child: Text(
+                      "경북멋쟁이 인증서",
                       style: TextStyle(
                         fontSize: 16.0,
-                        color: Colors.grey[700],
+                        fontFamily: 'KNUTRUTH',
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  const Center(
+                    child: Text(
+                      "홍길동",
+                      style: TextStyle(
+                        fontSize: 24.0,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Padding(
+                      padding: const EdgeInsets.all(32.0),
+                      child: Image.asset(
+                        'assets/images/knu_emblem.jpg',
+                        fit: BoxFit.contain,
+                      ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            );
-          },
+            ),
+          ),
         ),
       ),
     );
