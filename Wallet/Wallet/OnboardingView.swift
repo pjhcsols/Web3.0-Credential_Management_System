f53e473585b368784b1a688216b129a27b6b17ec
Seah Kim <seakim@knu.ac.kr>
2024-10-06 16:59:47 +0900

    feat: add animation for onboarding

--- Wallet/Wallet/OnboardingView.swift
+++ Wallet/Wallet/OnboardingView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    @State private var isVisible = false
+    
     var body: some View {
         VStack {
             Spacer()
             Text("간편한 공인인증서")
                 .font(.custom("KNU TRUTH", size: 36))
+                .opacity(isVisible ? 1 : 0)
+                .offset(y: isVisible ? 0 : 20)
+                .animation(.easeInOut(duration: 1).delay(0.3), value: isVisible)
+                .onAppear {
+                    isVisible = true
+                }
             Spacer()
             Button(action: {
             }) {
