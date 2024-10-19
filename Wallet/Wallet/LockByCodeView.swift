d09cd96227ca77bc3e354896d6d082dee1ca75d6
Seah Kim <seakim@knu.ac.kr>
2024-10-19 16:15:28 +0900

    feat: add navigation fot lock by code feature

--- Wallet/Wallet/LockByCodeView.swift
+++ Wallet/Wallet/LockByCodeView.swift
@@ -14,6 +14,7 @@ struct LockByCodeView: View {
     @State private var confirmCode: String = ""
     @State private var isConfirming: Bool = false
     @State private var showError: Bool = false
+    @State private var navigateToBio: Bool = false
 
     @FocusState private var isKeyboardFocused: Bool
 
@@ -25,66 +26,74 @@ struct LockByCodeView: View {
     ]
 
     var body: some View {
-        VStack {
-            Spacer()
-                .frame(height: 128)
+        NavigationStack {
+            VStack {
+                Spacer()
+                    .frame(height: 128)
 
-            Text(showError ? "비밀번호가 일치하지 않습니다.\n다시 입력해주세요." : (isConfirming ? "\n비밀번호를 한 번 더 입력해주세요" : "인증 시 사용할\n4자리 숫자를 입력해주세요"))
-            .font(.title3)
-            .fontWeight(.semibold)
-            .multilineTextAlignment(.center)
-            .foregroundColor(showError ? .red : .primary)
+                Text(showError ? "비밀번호가 일치하지 않습니다.\n다시 입력해주세요." : (isConfirming ? "\n비밀번호를 한 번 더 입력해주세요" : "인증 시 사용할\n4자리 숫자를 입력해주세요"))
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(showError ? .red : .primary)
 
-            HStack(spacing: 16) {
-                ForEach(0..<numberOfDigits, id: \.self) { index in
-                    ZStack {
-                        Rectangle()
-                            .fill(Color.white)
-                            .frame(width: 40, height: 50)
-                        if index < (isConfirming ? confirmCode.count : code.count) {
-                            Circle()
-                                .fill(Color.black)
-                                .frame(width: 16, height: 16)
-                        } else {
-                            Circle()
-                                .stroke(Color.black, lineWidth: 2)
-                                .frame(width: 16, height: 16)
-                        }
-                    }
-                }
-            }
-            .padding()
-
-
-
-            GeometryReader { geometry in
-                VStack {
-                    VStack(spacing: 8) {
-                        ForEach(keypadNumbers, id: \.self) { row in
-                            HStack(spacing: 16) {
-                                ForEach(row, id: \.self) { number in
-                                    Button(action: {
-                                        keypadButtonTapped(number)
-                                    }) {
-                                        if number == "⌫" && (isConfirming ? confirmCode.isEmpty : code.isEmpty) {
-                                            Color.clear
-                                                .frame(width: (geometry.size.width - 32) / 3, height: 80)
-                                        } else {
-                                            Text(number)
-                                                .font(.title)
-                                                .foregroundColor(Color.black)
-                                                .frame(width: (geometry.size.width - 32) / 3, height: 80)
-                                        }
-                                    }
-                                    .disabled(number.isEmpty)
-                                }
+                HStack(spacing: 16) {
+                    ForEach(0..<numberOfDigits, id: \.self) { index in
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.white)
+                                .frame(width: 40, height: 50)
+                            if index < (isConfirming ? confirmCode.count : code.count) {
+                                Circle()
+                                    .fill(Color.black)
+                                    .frame(width: 16, height: 16)
+                            } else {
+                                Circle()
+                                    .stroke(Color.black, lineWidth: 2)
+                                    .frame(width: 16, height: 16)
                             }
                         }
                     }
-                    Spacer()
+                }
+                .padding()
+
+                GeometryReader { geometry in
+                    let buttonWidth = max((geometry.size.width - 32) / 3, 0)
+                    VStack {
+                        VStack(spacing: 8) {
+                            ForEach(keypadNumbers, id: \.self) { row in
+                                HStack(spacing: 16) {
+                                    ForEach(row, id: \.self) { number in
+                                        Button(action: {
+                                            keypadButtonTapped(number)
+                                        }) {
+                                            if number == "⌫" && (isConfirming ? confirmCode.isEmpty : code.isEmpty) {
+                                                Color.clear
+                                                    .frame(width: buttonWidth, height: 80)
+                                            } else {
+                                                Text(number)
+                                                    .font(.title)
+                                                    .foregroundColor(Color.black)
+                                                    .frame(width: buttonWidth, height: 80)
+                                            }
+                                        }
+                                        .disabled(number.isEmpty)
+                                    }
+                                }
+                            }
+                        }
+                        Spacer()
+                    }
+                    .frame(height: 300)
+                }
+                .padding(.horizontal, 40)
+
+                NavigationLink(destination: LockByBioView()
+    .navigationBarBackButtonHidden(true),
+                               isActive: $navigateToBio) {
+                    EmptyView()
                 }
             }
-            .padding(.horizontal, 40)
         }
     }
 
@@ -120,10 +129,8 @@ struct LockByCodeView: View {
 
     private func validateCodes() {
         if code == confirmCode {
-            // 비밀번호가 일치하는 경우 처리
-            print("비밀번호가 일치합니다.")
+            navigateToBio = true
         } else {
-            // 비밀번호가 일치하지 않는 경우
             confirmCode = ""
             showError = true
         }
